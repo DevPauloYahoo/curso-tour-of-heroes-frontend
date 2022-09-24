750a03e494c632d6bfcffc9407ff01e6ba3d9386
PauloDev13 <devpgm2020@gmail.com>
2022-09-23 21:58:21 -0300

    Adding Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -8,7 +11,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
